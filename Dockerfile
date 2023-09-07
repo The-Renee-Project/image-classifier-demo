c0fdae28ba0e800457e170f99acfc19712db4707
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2023-09-06 20:50:15 -0700

    predownload.

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,7 @@ WORKDIR /app
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 COPY . .
+
+RUN python ./predownload.py
+
 CMD ["python3", "-m", "gunicorn", "-w", "4", "main:app"]
---
+++ predownload.py
@@ -0,0 +1,11 @@
+import os
+from PIL import Image
+from flask import Flask, request
+
+# For inference
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+model = ResNet50(weights='imagenet')
